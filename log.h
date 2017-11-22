263e08fe1baf6cf98554f48491c3d89e31fba02e
miel <mielvandam@gmail.com>
2017-11-22 09:31:41 +0100

    new log removed main2

--- log.h
+++ log.h
@@ -4,6 +4,7 @@
 #include <typeinfo>
 #include <cxxabi.h>
 #include <iterator>
+#include <iostream>
 #include <iomanip>
 #include <sys/time.h> // non-portable..
 
@@ -19,144 +20,61 @@
 
 namespace io {
 
-    std::ostream& red(std::ostream& os) {
-        os << RED;
-        return os;
-    }
-
-
-    std::ostream& reset(std::ostream& os) {
-        os << RESET;
-        return os;
-    }
-
-    template <typename T, typename S=std::char_traits<T> >
-    class basic_logstreambuf : public std::basic_streambuf<T, S> {
-        typedef S traits;
-        typedef typename traits::int_type int_type;
-        typedef std::basic_string<T, S> string_type;
-
-        //string_type pbuf;
-        //static const size_t BUFFER_SIZE=32;
-        //T pbuf[BUFFER_SIZE];
-
-        string_type pbuf;
-        string_type context;
-        string_type dtformat;
-
-        std::basic_streambuf<T, S>* sb;
-        typedef std::basic_streambuf<T, S> super_type;
-        using super_type::pbase;
-        using super_type::pbump;
-        using super_type::pptr;
-        using super_type::epptr;
-        using super_type::setp;
-		
-		std::string now() {
-    		timeval curTime;
-		    gettimeofday(&curTime, NULL);
-	    	int milli = curTime.tv_usec / 1000;
-	
-    		char buffer [80];
-		    strftime(buffer, 80, dtformat.c_str(), localtime(&curTime.tv_sec));
-
-	    	char currentTime[84] = "";
-    		sprintf(currentTime, "%s:%3d", buffer, milli);
-
-		    return std::string(currentTime);
-        }
-        
-
-    protected:
-
-        int stamp_and_flush(char* buf, int n) {
-
-            std::stringstream ss;
-            ss << "[" << MAGENTA << now() << RESET << "] " << context << "> ";
-            auto timestr = ss.str();
-            sb->sputn(&timestr.front(), timestr.length());
-            sb->sputn(buf, n); //&pbuf.front(), pbuf.size());
-
-        }
-        // overflow just grows the buffer in this case.
-        virtual int_type overflow(int_type c=traits::eof()) {
-            auto ch = traits::to_char_type(c);
-            auto n = pptr() - pbase();
-            pbuf.push_back(ch);
-            setp(&pbuf.front(), &pbuf.back() + 1);
-            pbump(n + 1);
-            return traits::not_eof(ch);
-        }
-
-
-        virtual int sync() {
-            if(pptr() + 1 >= epptr()) {
-                pbuf.push_back(static_cast<T>(0));
-                setp(&pbuf.front(), &pbuf.back() + 1);
-            } else {
-                *pptr() = static_cast<T>(0);
-            }
-
-            auto n = pptr() - pbase();
-
-            stamp_and_flush(&pbuf.front(), pbuf.size());
-            
-            return 0;
-        }
-
-    public:
-        basic_logstreambuf(const string_type& context, std::basic_streambuf<T, S>* out) 
-        : sb(out)
-        , context(context)
-        , dtformat("%Y-%m-%d %H:%M:%S")
-        {
-            setp(&pbuf.front(), &pbuf.back() + 1);
-        }
-
-        virtual ~basic_logstreambuf() {
-            if(pptr() != &pbuf.front()) {
-                sync();
-            }
-        }
-
-        void set_context(const string_type& c) {
-            context = c;
-        }
+    enum class loglevel : uint8_t {
+        quiet = 0,
+        error,
+        warn,
+        info,
+        all,
+        none
     };
 
-    typedef basic_logstreambuf<char> logstreambuf;
-
-
-    template <typename T, typename S=std::char_traits<T> >
-    class basic_logstream : public std::basic_ostream<T, S> {
-        typedef basic_logstreambuf<T, S> buffer_type;
-        typedef std::basic_ostream<T, S> super_type;
-        buffer_type buf;
-        //
-        //super_type& out;
+    class logstream : public std::ostringstream {
+        std::ostream& _out;
+        std::string _dtstring;
+        loglevel _curlevel;
     public:
-        basic_logstream(const std::string& context, super_type& os) 
-        //: out(os)
-        : super_type(&buf)
-        , buf(context, os.rdbuf()) 
-        {
-            this->write("enter\n", 7);
-            buf.pubsync();
-        }  
-
-        virtual ~basic_logstream() {
+        logstream(std::ostream& out) : _out(out), _dtstring("%Y%m%d %H:%M:%S") {
         }
 
-        /*template <typename R>
-        void value_of(const R& val, bool has_type) {
-        }*/
-    public:
+        virtual ~logstream() {
+            flush();
+        }
 
+        void flush() {
+            _out << str();
+            _out.flush();
+            str("");
+            _curlevel = loglevel::none;
+        }
+        /**
+        template <typename S> 
+        logstream& operator << (const S& t) {
+            (*reinterpret_cast<std::ostringstream*>(this)) << t;
+            return *this;
+        }*/
+
+        template <typename T>
+        inline logstream & operator<<( const T & t )
+        {
+            (*(std::ostringstream*)this) << t;
+            return *this;
+        }
+
+        /**
+        logstream& operator << (const loglevel& l) {
+            _curlevel = l;
+            return *this;
+        }*/
+
+        typedef logstream & (*logstream_manip)(logstream &);
+        logstream & operator<<(logstream_manip manip) { return manip(*this); }
+    };
 
         template <typename R>
         static std::string value_of(const std::string& name, const R& val, bool print_type=false) {
             std::stringstream ss;
-            ss << io::reset << name;
+            ss << RESET << name;
             if(print_type) {
                 int status = 0;
                 auto ti = typeid(val).name();
@@ -166,10 +84,17 @@ namespace io {
             ss << "=[" << BLUE << val << RESET << "]";
             return ss.str();
         }
-    };
 
-    typedef basic_logstream<char> logstream;
-#define DEBUG_METHOD()              io::logstream __logstream(__FUNCTION__, std::cout)
-#define DEBUG_VALUE_OF(x)           __logstream << io::logstream::value_of(#x, x, false) << std::endl;
-#define DEBUG_VALUE_AND_TYPE_OF(x)  __logstream << io::logstream::value_of(#x, x, true) << std::endl;
+
+    //logstream& operator << (logstream& out, logstream::level lvl);
+#define DEBUG_METHOD()              io::logstream __logstream(std::cout)
+#define DEBUG_VALUE_OF(x)           __logstream << io::value_of(#x, x, false) << std::endl
+#define DEBUG_VALUE_AND_TYPE_OF(x)  __logstream << io::value_of(#x, x, true) << std::endl
+}
+namespace std { 
+    io::logstream & endl(io::logstream & out) { 
+        out.put('\n'); 
+        out.flush(); 
+        return out; 
+    } 
 }
--- ndarray.h
+++ ndarray.h
@@ -7,26 +7,40 @@ namespace ndarray {
 
     template <typename T>
     auto row(std::valarray<T>& in, std::size_t i, std::size_t width) 
-    -> std::valarray<T> {
-        return std::valarray<T>(in[std::slice(i * width, width, 1)]);
+    -> std::slice_array<T> {
+        return in[std::slice(i * width, width, 1)];
     }
 
     template <typename T>
     auto column(std::valarray<T>& in, std::size_t i, std::size_t width) 
-        -> std::valarray<T> {
-        return std::valarray<T>(in[std::slice(i, in.size() / width, width)]);
+        -> std::slice_array<T> {
+        return in[std::slice(i, in.size() / width, width)];
     }
 
 
-    size_t indexnd(const std::valarray<std::size_t>& dim, const std::valarray<std::size_t>& ix) {
-        std::vector<size_t> fac;
-        std::partial_sum(std::begin(dim), std::end(dim) - 1, std::back_inserter(fac),
-                [&] (const size_t a, const size_t b) {
+    std::size_t indexnd(const std::valarray<std::size_t>& dim, const std::valarray<std::size_t>& ix) {
+        assert(dim.size() == ix.size());
+        // nd + Nd * (nd-1 + Nd-1 * (nd-2 + Nd-2 * (nd-3 ... ))) 
+#if 0 
+        std::size_t ii = 0;
+        std::size_t ml = 1;
+     
+        for(auto i: range(dim.size())) {
+            assert(ix[i] < dim[i]);
+            ii += ix[i] * ml; 
+            ml *= dim[i];
+        }
+        return ii;
+#else
+        std::vector<std::size_t> muls = { 1 };
+        std::partial_sum(std::begin(dim), std::end(dim) - 1, std::back_inserter(muls), 
+                [&] (const std::size_t& a, const std::size_t& b) {
                     return a * b;
                 });
-
-        return std::inner_product(std::begin(ix), std::end(ix), std::begin(fac), 0);
+        return std::inner_product(begin(ix), std::end(ix), std::begin(muls), 0);
+#endif
     }
+
 
     
 
@@ -35,6 +49,10 @@ namespace ndarray {
         return (a * b).sum();
     }
 
+    // @param - m rows of a
+    // @param - p cols of a <-> rows of b
+    // @param - q cols of b
+    // @returns p * q matrix
     template <typename T>
     auto matmul(const std::valarray<T>& a, const std::valarray<T>& b, int m, int p, int q) 
     -> std::valarray<T>
@@ -50,5 +68,23 @@ namespace ndarray {
         return ret;
     }
 
+    template <typename T>
+    std::valarray<T> colsum(std::valarray<T>& arr, unsigned dim) {
+        std::valarray<T> res(dim);
+        for(unsigned i=0; i < dim; i++) {
+            res[i] = static_cast<std::valarray<T> >(col(arr, i, dim)).sum(); 
+        }
+        return res;
+    }
+
+    template <typename T>
+    std::valarray<T> transposed(std::valarray<T>& arr, unsigned dim) {
+        std::valarray<T> res(arr.size());
+        unsigned rows = arr.size() / dim;
+        for(unsigned i=0; i < rows; i++) {
+            col(res, i, rows) = row(arr, i, dim);
+        }
+        return res;
+    }
 
 }
