4a402e34c91096ef1baff1313bf2128c387dea4b
miel <info@mimesis-games.com>
2017-11-23 12:55:06 +0100

    pre-merge

--- main2.cpp
+++ main2.cpp
@@ -30,209 +30,108 @@
 //#include "tensor.hpp"
 #define argmax(x)   std::max_element(((x)).begin(), ((x)).end())
 
-
 template <typename T>
-std::slice_array<T> row(std::valarray<T>& arr, unsigned r, unsigned dim) {
-    return arr[std::slice(r * dim, dim, 1)];
-}
+struct basic_ndarray {
+    typedef std::valarray<std::size_t> index_type;
+    typedef std::valarray<T> data_type;
 
-template <typename T>
-std::slice_array<T> col(std::valarray<T>& arr, unsigned c, unsigned dim) {
-    unsigned rows = arr.size() / dim;
-    return arr[std::slice(c, rows, dim)];
-}
+	index_type _shape;
+	data_type _data;
 
-template <typename T>
-std::valarray<T> colsum(std::valarray<T>& arr, unsigned dim) {
-    std::valarray<T> res(dim);
-    for(unsigned i=0; i < dim; i++) {
-        res[i] = static_cast<std::valarray<T> >(col(arr, i, dim)).sum(); 
+//public:
+
+	std::slice_array<T> row(std::size_t ix) {
+	    return _data[std::slice(ix * shape[1], shape[1], 1)];
+	}
+
+	std::slice_array<T> column(unsigned ix) {
+	    unsigned iy = shape[0];
+    	return _data[std::slice(ix, iy, shape[1])];
+	}
+
+	static
+	std::valarray<T> transposed(std::valarray<T>& arr, unsigned dim) {
+	    std::valarray<T> res(arr.size());
+    	unsigned rows = arr.size() / dim;
+	    for(unsigned i=0; i < rows; i++) {
+    	    column(res, i, rows) = row(arr, i, dim);
+	    }
+    	return res;
+	}
+    
+	std::valarray<T> matmul(std::valarray<T>& a, std::valarray<T>& b, unsigned m, unsigned p, unsigned q) {
+    	DEBUG_METHOD();
+	    std::valarray<T> c(m * q);
+    	for(auto i=0; i < m; i++) {
+        	const auto& yy = static_cast<std::valarray<T> >(a[std::slice(i * p, p, 1)]);
+	        for(auto j=0; j < q; j++) {
+    	        const auto& xx = static_cast<std::valarray<T> >(b[std::slice(j, p, q)]);
+        	    c[j + i * q] = (xx * yy).sum();
+	        }
+    	}
+	    return c;
+	}
+
+
+	void identity(std::valarray<T>& a, unsigned dim) {
+    	std::fill(std::begin(a), std::end(a), 0.0f);
+	    a[std::slice(0, a.size(), dim + 1)] = 1.0f; 
+	}
+
+	T trace(const std::valarray<T>& a, unsigned dim) {
+    	return a[std::gslice(0, {dim}, {dim+1})].sum();
+	}
+
+
+	std::size_t indexnd(const std::valarray<std::size_t>& ix) {
+    	assert(_shape.size() == ix.size());
+	    // nd + Nd * (nd-1 + Nd-1 * (nd-2 + Nd-2 * (nd-3 ... ))) 
+#if 0 
+    	std::size_t ii = 0;
+	    std::size_t ml = 1;
+     
+    	for(auto i: range(_shape.size())) {
+        	assert(ix[i] < _shape[i]);
+	        ii += ix[i] * ml; 
+    	    ml *= dim[i];
+    	}
+	    return ii;
+#else
+	    std::vector<std::size_t> muls = { 1 };
+    	std::partial_sum(std::begin(_shape), std::end(_shape) - 1, std::back_inserter(muls), 
+        	    [&] (const std::size_t& a, const std::size_t& b) {
+            	    return a * b;
+	            });
+    	return std::inner_product(begin(ix), std::end(ix), std::begin(muls), 0);
+#endif
+	}
+
+    inline const index_type shape() const {
+        return shape;
     }
-    return res;
-}
 
-template <typename T>
-std::valarray<T> transposed(std::valarray<T>& arr, unsigned dim) {
-    std::valarray<T> res(arr.size());
-    unsigned rows = arr.size() / dim;
-    for(unsigned i=0; i < rows; i++) {
-        col(res, i, rows) = row(arr, i, dim);
-    }
-    return res;
-}
+};
 
-template <typename T>
-void identity(std::valarray<T>& a, unsigned dim) {
-    std::fill(std::begin(a), std::end(a), 0.0f);
-    a[std::slice(0, a.size(), dim + 1)] = 1.0f; 
-}
 
-template <typename T>
-T trace(const std::valarray<T>& a, unsigned dim) {
-    return a[std::gslice(0, {dim}, {dim+1})].sum();
-}
 
 template <typename T>
 std::ostream& operator << (std::ostream& os, const std::valarray<T>& a) {
-    std::copy(std::begin(a), std::end(a) - 1, std::ostream_iterator<float>(os, ","));
-    os << *(std::end(a) - 1);
+    std::copy(std::begin(a), std::prev(std::end(a)), std::ostream_iterator<float>(os, ","));
+    os << *std::prev(std::end(a));
     return os;
 }
 
-template<template<typename T> class U, typename T>
-std::ostream& operator << (std::ostream& out, const U<T>& x)
-{
-    out << static_cast<std::valarray<T> >(x);
-    return out;
-}
-
-
-template <typename T, std::size_t R>
-class ndarray {
-    std::valarray<T> data;
-    std::valarray<std::size_t> dim;
-public:
-    typedef std::valarray<std::size_t> index_type;
-    ndarray(const index_type& dims) {
-        DEBUG_METHOD();
-        auto data_size = std::accumulate(std::begin(dims), std::end(dims), 1, std::multiplies<T>());
-        DEBUG_VALUE_OF(data_size);
-        data = std::valarray<T>(data_size);
-        dim = dims;
-        std::iota(std::begin(data), std::end(data), 0.0f);
-        DEBUG_VALUE_OF(data);
-    }
-
-    std::size_t compute_index(const index_type& ix) const {
-        DEBUG_METHOD();
-        DEBUG_VALUE_OF(ix);
-
-        assert(R == ix.size());
-        
-        std::size_t ii = 0;
-        std::size_t ml = 1;
-
-        for(auto i: range(dim.size())) {
-            assert(ix[i] < dim[i]);
-            ii += ix[i] * ml; 
-            DEBUG_VALUE_OF(ii);
-            ml *= dim[i];
-        }
-        // nd + Nd * (nd-1 + Nd-1 * (nd-2 + Nd-2)
-        return ii;
-    }
-};
-
 template <typename T>
-std::valarray<T> tensordot(const std::valarray<T>& a,
-        const std::valarray<T>& b,
-        const std::valarray<std::size_t>& dim_a,
-        const std::valarray<std::size_t>& dim_b)
-{
-
-}
-
-std::size_t indexnd(const std::valarray<std::size_t>& dim, const std::valarray<std::size_t>& ix) {
-    assert(dim.size() == ix.size());
-    // nd + Nd * (nd-1 + Nd-1 * (nd-2 + Nd-2 * (nd-3 ... ))) 
-#if 0 
-    std::size_t ii = 0;
-    std::size_t ml = 1;
-     
-    for(auto i: range(dim.size())) {
-        assert(ix[i] < dim[i]);
-        ii += ix[i] * ml; 
-        ml *= dim[i];
-    }
-    return ii;
-#else
-    std::vector<std::size_t> muls = { 1 };
-    std::partial_sum(std::begin(dim), std::end(dim) - 1, std::back_inserter(muls), 
-            [&] (const std::size_t& a, const std::size_t& b) {
-                return a * b;
-            });
-    return std::inner_product(begin(ix), std::end(ix), std::begin(muls), 0);
-#endif
+std::ostream& operator << (std::ostream& os, const basic_ndarray<T>& va) {
+    return os;
 }
 
 #include <fstream>
 #include "csv.hpp"
-#include "matlab.hpp"
-
-template <typename T>
-void print_asmatrix(std::valarray<T>& a, unsigned dim) {
-    DEBUG_METHOD();
-    unsigned rb = a.size() / dim;
-    for(auto i : range(rb)) {
-        const auto& row = a[std::slice(i * dim, dim, 1)];
-        DEBUG_VALUE_OF(row);
-    }
-}
-
-// @param - m rows of a
-// @param - p cols of a <-> rows of b
-// @param - q cols of b
-// @returns p * q matrix
-template <typename T>
-std::valarray<T> matmul(std::valarray<T>& a, std::valarray<T>& b, unsigned m, unsigned p, unsigned q) {
-    DEBUG_METHOD();
-    std::valarray<T> c(m * q);
-    for(auto i=0; i < m; i++) {
-        const auto& yy = static_cast<std::valarray<T> >(a[std::slice(i * p, p, 1)]);
-        for(auto j=0; j < q; j++) {
-            const auto& xx = static_cast<std::valarray<T> >(b[std::slice(j, p, q)]);
-            c[j + i * q] = (xx * yy).sum();
-        }
-    }
-    return c;
-}
-
-template <typename T>
-T mean(const std::valarray<T>& a) {
-    return a.sum() / a.size();
-}
 
 
 int main(int argc, char* argv[]) {
     DEBUG_METHOD();
-
-    std::valarray<std::size_t> v = { 4, 4, 1 };
-    DEBUG_VALUE_OF(indexnd(v, {3,3,0}));
-
-    std::valarray<float> a(60);
-    std::valarray<std::size_t> dim_a = { 3, 4, 5 };
-    std::iota(std::begin(a), std::end(a), 0.0f);
-
-    std::valarray<float> b(24);
-    std::valarray<std::size_t> dim_b = { 4, 3, 2 };
-    std::iota(std::begin(b), std::end(b), 0.0f);
-
-    std::valarray<float> d(10);
-    std::valarray<std::size_t> dim_d = { 5, 2 };
-    std::fill(std::begin(d), std::end(d), 0.0f);
-
-    for(auto i: range(5)) {
-        for(auto j: range(2)) {
-            for(auto k: range(3)) {
-                for(auto n: range(4)) {
-                    float x = a[indexnd({dim_a}, {k, n, i})];
-                    float y = b[indexnd({dim_b}, {n, k, j})];
-                    DEBUG_VALUE_OF(std::vector<int>({k, n, i, (int)x}));
-                    DEBUG_VALUE_OF(std::vector<int>({n, k, j, (int)y}));
-
-                    d[i + j * 2] += x * y;
-                    //DEBUG_VALUE_AND_TYPE_OF(d);
-                }
-            }
-        }
-    } 
-
-
-
-    DEBUG_VALUE_AND_TYPE_OF(d);
-
-    //cs231n();
-	/**
     typedef std::tuple<float, float> row_type;
     std::vector<row_type> data;
     std::ifstream is("data/ex1data1.txt");
@@ -242,20 +141,23 @@ int main(int argc, char* argv[]) {
 		}
     }
 
+
+
     
-    DEBUG_VALUE_AND_TYPE_OF(data);
+#if 1
+    //DEBUG_VALUE_AND_TYPE_OF(data);
 
-    std::valarray<float> x(data.size() * 2);
-
-
+#else
+    std::valarray<float> X(data.size() * 2);
+    basic_ndarray<float> x = { { data.size(), 2 }, X };
     std::for_each(data.begin(), data.end(), [&] (const row_type& row) {
             static int i=0;
-            x[i++] = std::get<0>(row);
-            x[i++] = std::get<1>(row);
+            x._data[i++] = std::get<0>(row);
+            x._data[i++] = std::get<1>(row);
     });
-
-    print_asmatrix(x, 2);
-	*/
+    //print_asmatrix(x, 2);
+    DEBUG_VALUE_OF(x);
+#endif
     return 0;
 	
 
